6be4c0ecdfaaecdb77f0cbc74240a0372b4f3f1c
sravaniyalla09 <Sravani.yalla09@gmail.com>
2026-01-02 15:13:07 +0530

    adding the changes

---
+++ my-java-app/src/main/java/com/example/app.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("------------------------------------");
+        System.out.println("Hello! Java App is running in Docker");
+        System.out.println("------------------------------------");
+    }
+}
